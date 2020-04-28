84cd37ab3a57b8a7e64df3f5b0ab88b5257c831a
whoamiLiinux <riveraant1975@gmail.com>
2020-04-27 20:42:49 -0500

    Upated

--- Receptor/src/app/App.java
+++ Receptor/src/app/App.java
@@ -3,23 +3,38 @@ package app;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.print("Introduce el Dato: ");
-        String DatoP; //Declara el dato que se va a pedir
-        Scanner DatoScan=new Scanner(System.in); //Se pide el dato
-        DatoP=DatoScan.nextLine();//Se guarda el dato
+    public static void main(final String[] args) {
+
+        System.out.print("Introduce un caracter: ");
+        String LetraP; //Declara el caracter que se va a pedir
+        final Scanner LetraScan = new Scanner(System.in); //Se pide el caracter
+        LetraP = LetraScan.nextLine(); //Se guarda el caracter
+        System.out.println(convBinario(LetraP.charAt(0))); //Pasa a binario);
+        
+   
         System.out.print("Introduce el divisor: ");
         String DivisorP;
-        Scanner DivisorScan=new Scanner(System.in);
-        DivisorP=DivisorScan.nextLine();
+        final Scanner DivisorScan = new Scanner(System.in);
+        DivisorP = DivisorScan.nextLine();
+
         System.out.println("Seleccione el tipo de Paridad");
         System.out.println("1. Par");
         System.out.println("2. Impar");
         System.out.print("Opcion: ");
+
+        //Opciones
         int OpcP;
-        Scanner OpcScan=new Scanner(System.in);
-        OpcP=OpcScan.nextInt();
-        switch(OpcP){
+        final Scanner OpcScan = new Scanner(System.in);
+        menu(OpcP=OpcScan.nextInt());
+
+
+    }
+
+
+
+    static public void menu(final int _opc) { // Menu opciones
+        
+        switch(_opc){
             case 1:
                 
                 break;
@@ -29,5 +44,16 @@ public class App {
             default:
                 System.out.println("Opcion no valida");
         }
+
+    }
+
+    static public String  convBinario(int valorAscii){ //A binario
+           
+            String numBinario = Integer.toBinaryString(valorAscii); 
+            String aux ="0";
+            if(numBinario.length()<8){
+                return aux+=numBinario;
+            }
+            return   numBinario;
     }
 }
